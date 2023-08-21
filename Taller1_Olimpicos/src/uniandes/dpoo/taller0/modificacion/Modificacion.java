32a8e0652a4d396a4dca9b23d55d69c4cd5fba5a
acrodriguezs1 <123606873+acrodriguezs1@users.noreply.github.com>
2023-08-20 20:24:18 -0500

    Add files via upload

---
+++ Taller1_Olimpicos/src/uniandes/dpoo/taller0/modificacion/Modificacion.java
@@ -0,0 +1,26 @@
+package uniandes.dpoo.taller0.modificacion;
+
+import java.io.IOException;
+
+import uniandes.dpoo.taller0.procesamiento.CalculadoraEstadisticas;
+import uniandes.dpoo.taller0.procesamiento.LoaderOlimpicos;
+
+public class Modificacion {
+	
+	public static void main(String[] args)
+	{
+		try
+		{
+		System.out.println("Hola, mundo!");
+		CalculadoraEstadisticas calc = LoaderOlimpicos.cargarArchivo("./data/atletas.csv");
+		System.out.println(calc.paisConMasMedallistas());
+		}
+		catch(IOException e)
+		{
+			System.out.println("Error leyendo de la consola");
+			e.printStackTrace();
+		}
+		
+	}
+}
+	
